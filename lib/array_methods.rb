bd1adada44c63e0e5e982118c706e7e7e69e149a
Yining Wang <yining.wang@utexas.edu>
2019-10-17 22:21:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = array[0]
+  counter = 1
+  while array[counter] do
+    min = array[counter] if array[counter] < min
+    counter += 1
+  end
+  min
 end
