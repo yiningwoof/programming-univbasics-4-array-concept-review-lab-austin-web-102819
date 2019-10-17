528f3ea4bf2d5f18847b262e48b81ee2fc20eff8
Yining Wang <yining.wang@utexas.edu>
2019-10-17 22:30:49 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,31 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array[counter] do
+    if array[counter] == value_to_find
+      index = counter 
+      break
+    end
+    counter += 1
+  end
+  index
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = array[0]
+  counter = 1
+  while array[counter] do
+    max = array[counter] if array[counter] > max
+    counter += 1
+  end
+  max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = array[0]
+  counter = 1
+  while array[counter] do
+    min = array[counter] if array[counter] < min
+    counter += 1
+  end
+  min
 end
