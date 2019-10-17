efa545e31e5e256c4c207075145f8699e2f70f89
Yining Wang <yining.wang@utexas.edu>
2019-10-17 22:14:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   counter = 0
-  while counter < array.length do
+  while array[counter] do
     if array[counter] == value_to_find
       break
     counter += 1
