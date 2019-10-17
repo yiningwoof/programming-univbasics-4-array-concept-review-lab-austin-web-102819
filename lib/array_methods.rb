73c50d3d5a45e159845b1c9f6ee77b35c44caf6c
Yining Wang <yining.wang@utexas.edu>
2019-10-17 22:06:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
 end
 
 def find_min_value(array)
